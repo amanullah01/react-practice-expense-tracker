44b2b57d1a2134a9381690e0136ca24bad25940d
Amanullah Aman <amancse.aiub@gmail.com>
2022-11-04 20:11:52 +0600

    data show dynamicaly

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 8, 28);
+  const expenseTitle = "Air Fare";
+  const expensePrice = 100.8;
   return (
     <div className="expense-item">
-      <div>29th September, 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Air Fare</h2>
-        <div className="expense-item__price">$100.80</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expensePrice}</div>
       </div>
     </div>
   );
