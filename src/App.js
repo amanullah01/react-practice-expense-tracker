ca014272c817ea32a15854c9ee3e3100b7ce9d79
Amanullah Aman <amancse.aiub@gmail.com>
2022-11-05 13:44:35 +0600

    data child to parent parsing

--- src/App.js
+++ src/App.js
@@ -9,9 +9,14 @@ const App = () => {
     { id: 3, title: "Air fare", amount: 940.54, date: new Date(2022, 10, 3) },
     { id: 4, title: "Book", amount: 79.99, date: new Date(2022, 10, 4) },
   ];
+
+  const onAddExpenseHandler = (expense) => {
+    console.log("app.js");
+    console.log(expense);
+  };
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={onAddExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
