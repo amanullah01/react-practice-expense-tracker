53011552177d8132f2689a3f562bb19cd85e6e68
Amanullah Aman <amancse.aiub@gmail.com>
2022-11-06 08:52:39 +0600

    dynamically data added to the list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
+const DUMMY_EXPENSES = [
+  { id: 1, title: "Car wash", amount: 40, date: new Date(2022, 10, 1) },
+  { id: 2, title: "Car insurance", amount: 240, date: new Date(2022, 10, 2) },
+  { id: 3, title: "Air fare", amount: 940.54, date: new Date(2022, 10, 3) },
+  { id: 4, title: "Book", amount: 79.99, date: new Date(2022, 10, 4) },
+];
+
 const App = () => {
-  const expenses = [
-    { id: 1, title: "Car wash", amount: 40, date: new Date(2022, 10, 1) },
-    { id: 2, title: "Car insurance", amount: 240, date: new Date(2022, 10, 2) },
-    { id: 3, title: "Air fare", amount: 940.54, date: new Date(2022, 10, 3) },
-    { id: 4, title: "Book", amount: 79.99, date: new Date(2022, 10, 4) },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const onAddExpenseHandler = (expense) => {
-    console.log("app.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...expenses];
+    });
   };
   return (
     <div>
